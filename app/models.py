dbe1d447d7904d64f67c8e7d0e77543ee8212f30
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-08-09 14:54:54 -0400

    Add model for MonsterLog, MonsterCategories.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,25 @@
 from app import db
+
 class Monster(db.Model):
     __tablename__ = "monster_info"
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String,nullable=False)
+    name = db.Column(db.String,nullable=False,unique=True)
 
+"""
+Monster - (Foreign Key)
+amount - integer
+category - integer/string
+"""
+class MonsterCategories(db.Model):
+    __table__name = "monster_category"
+    id = db.Column(db.Integer,primary_key=True)
+    category = db.Column(db.String,unique=True)
 
-# monster log 
+class MonsterLog(db.Model):
+    __table__name = "monster_logs"
+    id=db.Column(db.Integer,primary_key=True)
+    amount = db.Column(db.Integer)
+    # foreign key to monster
+    monster_id = db.Column(db.Integer,db.ForeignKey('monster_id'),nullable=False)
+    # forgien key to monstercategory 
+    monster_category_id = db.Column(db.Integer,db.ForeignKey('monstercategories_id',nullable=False))
--- tests/test_monsters.py
+++ tests/test_monsters.py
@@ -5,22 +5,27 @@ import unittest
 sys.path.append('./')
 from app.models import Monster,db
 from base import BaseTestCase
+from sqlalchemy.exc import IntegrityError
 
 
 TEST_DB = "test.db"
 
+def create_monster(entered_name):
+    db.session.add(Monster(name=entered_name))
+    db.session.commit()
+
 class TestMonsterModel(BaseTestCase):
 
     def test_create_monster_data(self):
-        mnstr = Monster(name="test")
-
-        db.session.add(mnstr)
-        db.session.commit()
-
+        create_monster("test")
         monster = Monster.query.get(1)
         total_monsters = Monster.query.all()
         self.assertEqual(monster.name,"test")
         self.assertEqual(len(total_monsters),1)
+
+    def test_monster_data_unique_constraint(self):
+        create_monster("test")
+        self.assertRaises(IntegrityError,create_monster,"test")
 
 if __name__ == "__main__":
     unittest.main()
