f5911ab1370677102167a92e06e3d7fc3f7a7146
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-08-12 21:26:18 -0400

    Fix requirements.txt and fix duplicate monster entries in db.

--- app/utils.py
+++ app/utils.py
@@ -1,19 +1,14 @@
-from osrsbox import monsters_api
 from .models import Monster,db
 
 class PersistToDB:
+
     def add_to_db(self,item):
         db.session.add(item)
         db.session.commit()
 
-class OSRSMonsters(PersistToDB):
-    def __init__(self):
-        self.monsters = monsters_api.load()
 
-    def loop_through(self):
-        for monster in self.monsters:
-            monster = self.create_monster(monster.name,monster.combat_level,monster.id)
-            self.add_to_db(monster)
+class OSRSMonsters(PersistToDB):
 
     def create_monster(self,monster_name,combat_level,monster_id):
         return Monster(name=monster_name,level=combat_level,monster_id=monster_id)
+
--- manager.py
+++ manager.py
@@ -3,8 +3,10 @@ import unittest
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 from flask import url_for
+from osrsbox import monsters_api
 
 from app.models import db
+from app.utils import OSRSMonsters
 import app
 
 app = app.create_app()
@@ -20,6 +22,19 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@manager.command
+def create_monsters_db():
+    monsters = monsters_api.load()
+    saved_to_db = {}
+    osrs_monsters = OSRSMonsters()
+    for monster in monsters:
+        if monster.name in saved_to_db and saved_to_db[monster.name]['combat_level'] == monster.combat_level:
+            continue
+        mon = osrs_monsters.create_monster(monster.name,monster.combat_level,monster.id)
+        osrs_monsters.add_to_db(mon)
+
+        saved_to_db[monster.name] = {"combat_level":monster.combat_level}
 
 
 if __name__ == "__main__":
